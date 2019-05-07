1366d5c5d955fe673a12dadc31ddd1e359776b43
Shirokovskikh Sergey <hellomoto177@yandex.ru>
2019-05-07 11:15:07 +0500

    let's move to travis!

--- ormconfig.js
+++ ormconfig.js
@@ -1,27 +1,15 @@
 require('dotenv/config');
 
 const isTestEnv = process.env.NODE_ENV === 'test';
-// const databaseOpts = {
-//   type: 'postgres',
-//   username: isTestEnv ? process.env.TEST_DB_USER : process.env.DB_USER,
-//   password: isTestEnv ? process.env.TEST_DB_PASSWORD : process.env.DB_PASSWORD,
-//   host: isTestEnv ? process.env.TEST_DB_HOST : process.env.DB_HOST,
-//   port: Number(isTestEnv ? process.env.TEST_DB_PORT : process.env.DB_PORT),
-//   database: isTestEnv ? process.env.TEST_DB_NAME : process.env.DB_NAME,
-//   entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
-//   migrationsRun: isTestEnv,
-//   migrations: [__dirname + '/src/migrations/*.ts'],
-//   synchronize: false,
-// };
 const databaseOpts = {
   type: 'postgres',
-  username: 'user',
-  password: 'password',
-  host: 'localhost',
-  port: 5442,
-  database: 'english_test',
+  username: isTestEnv ? process.env.TEST_DB_USER : process.env.DB_USER,
+  password: isTestEnv ? process.env.TEST_DB_PASSWORD : process.env.DB_PASSWORD,
+  host: isTestEnv ? process.env.TEST_DB_HOST : process.env.DB_HOST,
+  port: Number(isTestEnv ? process.env.TEST_DB_PORT : process.env.DB_PORT),
+  database: isTestEnv ? process.env.TEST_DB_NAME : process.env.DB_NAME,
   entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
-  migrationsRun: true,
+  migrationsRun: isTestEnv,
   migrations: [__dirname + '/src/migrations/*.ts'],
   synchronize: false,
 };
