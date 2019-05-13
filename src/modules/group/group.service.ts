6d4b4b70f77fb07b96f3539656950dd11c9e02be
Shirokovskikh Sergey <hellomoto177@yandex.ru>
2019-05-13 23:07:55 +0500

    correct imports

--- src/modules/group/group.service.ts
+++ src/modules/group/group.service.ts
@@ -2,8 +2,7 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Group } from './group.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import CreateGroupDTO from './dto/create-group.dto';
-import UpdateGroupDTO from './dto/update-group.dto';
+import { CreateGroupDTO, UpdateGroupDTO } from './group.dto';
 
 @Injectable()
 export class GroupService {
