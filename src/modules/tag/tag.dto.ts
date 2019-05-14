6278fed3123f1abb824f55c79c8af781aa07f354
Shirokovskikh Sergey <hellomoto177@yandex.ru>
2019-05-14 10:19:50 +0500

    fix tag dto

--- src/modules/tag/tag.dto.ts
+++ src/modules/tag/tag.dto.ts
@@ -1,17 +1,22 @@
 import { IsDefined } from 'class-validator';
 import { ResponseNoteDTO } from '../note/note.dto';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class ResponseTagDTO {
+  @ApiModelProperty({ type: String })
   name: string;
+  @ApiModelProperty({ type: ResponseNoteDTO, isArray: true })
   notes?: ResponseNoteDTO[];
 }
 
 export class CreateTagDTO {
   @IsDefined()
+  @ApiModelProperty({ type: String })
   name: string;
 }
 
 export class UpdateTagDTO {
   @IsDefined()
+  @ApiModelProperty({ type: String })
   name: string;
 }
